341699eae04d8e585c6c127014209eafe530e22d
anarbek <balmukanovab@gmail.com>
2021-08-12 20:04:05 +0500

    Удалил бд из проекта за не надобностью

--- src/main/java/ru/balmukanov/palantir/adapter/jpa/SiteJpaRepository.java
+++
@@ -1,7 +0,0 @@
-package ru.balmukanov.palantir.adapter.jpa;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.balmukanov.palantir.domain.Site;
-
-public interface SiteJpaRepository extends JpaRepository<Site, Long> {
-}
